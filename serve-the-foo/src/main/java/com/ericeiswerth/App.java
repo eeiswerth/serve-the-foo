b2a9c840605dfe4336e111a6d547ccc1dfda298e
Eric Eiswerth <eric.eiswerth@gmail.com>
2015-05-07 20:54:41 -0400

    Added logback logging.

---
+++ serve-the-foo/src/main/java/com/ericeiswerth/App.java
@@ -0,0 +1,18 @@
+package com.ericeiswerth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        LOGGER.info("Hello World!");
+    }
+}
